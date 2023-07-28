84e82ee1d43fb5dfe29ec4c6803f6a02805a39fa
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-07-28 19:23:40 +0700

    finish 3.12 mongoDB

---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const database = 'phonebook'
+
+const url =
+  `mongodb+srv://hasferrr:${password}@cluster0.1swslkp.mongodb.net/${database}?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+// Define schema
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+// Define model
+const Person = mongoose.model('Person', personSchema)
+
+// Fetch all
+if (process.argv.length === 3) {
+  Person
+    .find({})
+    .then(persons => {
+      console.log('phonebook:')
+      persons.forEach(person => {
+        console.log(person.name, person.number)
+      })
+      mongoose.connection.close()
+    })
+}
+
+// Add phonebook
+if (process.argv.length === 5) {
+  const person = new Person({
+    "name": process.argv[3],
+    "number": process.argv[4]
+  })
+
+  person
+    .save()
+    .then(addedPerson => {
+      console.log(`added ${addedPerson.name} number ${addedPerson.number} to phonebook`)
+      mongoose.connection.close()
+    })
+}
