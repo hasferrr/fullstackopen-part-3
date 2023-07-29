a171562add7ad045e86fb5c66dae2cd7b8d1e7f0
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-07-29 16:13:10 +0700

    finish 3.18

--- index.js
+++ index.js
@@ -9,28 +9,6 @@ app.use(express.static('build'))
 morgan.token('req-body', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : '')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
 
 app.get('/api/persons', (req, res) => {
   Person
@@ -41,18 +19,24 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-  res.send(`<p>Phonebook has info for ${persons.length} people</p>
-  <p>${Date()}</p>`)
+  Person
+    .count()
+    .then(num => {
+      res.send(`<p>Phonebook has info for ${num} people</p><p>${Date()}</p>`)
+    })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person
+    .findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
@@ -69,12 +53,6 @@ app.post('/api/persons', (req, res) => {
   if (!newPerson.name || !newPerson.number) {
     return res.status(400).json({
       error: 'name or number is missing'
-    })
-  }
-
-  if (persons.find(person => person.name === newPerson.name)) {
-    return res.status(400).json({
-      error: 'name must be unique'
     })
   }
 
